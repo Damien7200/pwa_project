5a2141e94000ec827cd159457987e07c200a534c
Damien7200 <damienwaldon@gmail.com>
2025-05-01 20:54:21 +1000

    Week 9 completed - Implementation of  ORM

--- users/views.py
+++ users/views.py
@@ -22,6 +22,7 @@ def login_view(request):
     return render(request, "users/login.html")
 
 def logout_view(request):
+    
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect('users:login')
